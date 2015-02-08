1de23685742f080d868ab0fc06161f12e621d9ae
cnvill <cn_vill@hotmail.com>
2015-02-08 22:56:38 +0000

    Optimization Code

--- NotifyServer/src/com/cn/notifyserver/MainActivity.java
+++ NotifyServer/src/com/cn/notifyserver/MainActivity.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 public class MainActivity extends Activity
 {
-     private DataBaseManager manager;
+    private DataBaseManager manager;
     Cursor cursor;
     private ListView lista, lv;
     SimpleCursorAdapter adapter;
@@ -31,14 +31,12 @@ public class MainActivity extends Activity
     private ProgressDialog progress;
     Handler updateBarHandler;
 
-    private void finish(View v){
-        finish();
-    }
-
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
         manager= new DataBaseManager(this);
         dlConfirmacion = new AlertDialog.Builder(this);
         updateBarHandler = new Handler();
@@ -50,12 +48,6 @@ public class MainActivity extends Activity
         txtBuscar= (EditText) findViewById(R.id.txtBuscar);
         lista= (ListView) findViewById(R.id.lvLista);
 
-
-        btnbuscar=(ImageButton) findViewById(R.id.btnBuscar);
-        btnNuevo=(ImageButton) findViewById(R.id.btnNuevo);
-
-        cursor = manager.cargarCursorContactos();
-
         String[] from = new String[]{ manager.ptName, manager.ptTelefono};
         int[] to = new int[]{android.R.id.text1, android.R.id.text2 };
         adapter= new  SimpleCursorAdapter(this, android.R.layout.two_line_list_item, cursor, from, to, 0);
@@ -65,7 +57,8 @@ public class MainActivity extends Activity
         dlConfirmacion.setMessage("¿ Estas seguro de enviar una petición ?");
         dlConfirmacion.setCancelable(false);
 
-        AlertDialog.Builder aceptar = dlConfirmacion.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+        AlertDialog.Builder aceptar = dlConfirmacion.setPositiveButton("Aceptar", 
+        new DialogInterface.OnClickListener() {
         public void onClick(DialogInterface dialogo, int id) {
 
                 progress = new ProgressDialog(MainActivity.this);
@@ -78,45 +71,19 @@ public class MainActivity extends Activity
                     Cursor c = manager.buscarContactoId(contactoId);
                     if (c.moveToFirst()) {
                         numeroEnvio = c.getString(2);
+                        Toast.makeText(getApplicationContext(), " Enviando petición ...", Toast.LENGTH_SHORT).show();
                         cgeneral.sendSMS(numeroEnvio, ".");
                         numeroEnvio = "+51" + numeroEnvio;
-
                         new EnvioRecepcionSms().execute();
                     }
                 } catch (Exception e) {
                     Toast.makeText(getApplicationContext(), "Error al solicitar " + e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
-
-               //new Thread(new Runnable() {
-               /* final Thread t = new Thread(){
-                    @Override
-                    public void run() {
-                        try {
-                            while (progress.getProgress() <= 100) {
-                                Thread.sleep(1500);
-                                updateBarHandler.post(new Runnable() {
-                                    public void run() {
-                                        progress.incrementProgressBy(5);
-                                    }
-                                });
-
-                                if (progress.getProgress() == progress.getMax()) {
-                                    progress.dismiss();
-                                    break;
-                                }
-                            }
-                        } catch (Exception ex) {
-                            Toast.makeText(getApplicationContext(), "Error al solicitar " + ex.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                };
-
-                t.start();*/
-
-                //Toast.makeText(getApplicationContext(), "Finalizo", Toast.LENGTH_SHORT).show();
             }
         });
-        dlConfirmacion.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+        
+        dlConfirmacion.setNegativeButton("Cancelar", 
+        new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialogo, int id) {
                 Toast.makeText(getApplicationContext(), "Se cancelo la petición", Toast.LENGTH_SHORT).show();
             }
@@ -124,7 +91,7 @@ public class MainActivity extends Activity
 
         lista.setOnItemClickListener(new OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                contactoId=String.valueOf(id);
+                contactoId = String.valueOf(id);
                 dlConfirmacion.show();
             }
         });
@@ -144,30 +111,27 @@ public class MainActivity extends Activity
     public void NuevoContacto(View v){
         Intent nuevo=new Intent(this, NewContact.class);
         startActivity(nuevo);
-
     }
 
-    public void EnvioRecepcionASms(){
-        smsRespuesta=cgeneral.readSMS();
-        String[] respuesta = smsRespuesta.split(";");
-        //Toast.makeText(getApplicationContext(), "Respuesta", Toast.LENGTH_SHORT).show();
-        //int pTelefono = respuesta.indexOf(','), pLatitud=respuesta.indexOf(';');
-       if(respuesta.length == 3){
-            numeroRetorno =respuesta[0] ; //smsRespuesta.substring(0, pTelefono);
-            latitud = respuesta[1]; //Double.parseDouble(smsRespuesta.substring(pTelefono+1, pLatitud));
-            longitud = respuesta[2]; //Double.parseDouble(smsRespuesta.substring(pLatitud+1));
-            if(numeroEnvio.trim().equalsIgnoreCase(numeroRetorno.trim())) {
-                
-                //Toast.makeText(getApplicationContext(), " Nro " + numeroRetorno + " lat:" + latitud + " Long:" + longitud, Toast.LENGTH_SHORT).show();
-            }
-            //else
-              //  Toast.makeText(getApplicationContext(), "Se excedio el tiempo de respuesta no se puedo recuperar la posición", Toast.LENGTH_SHORT).show();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.item) {
+            return true;
         }
-        //else
-          //  Toast.makeText(getApplicationContext(), "Se excedio el tiempo de respuesta no se puedo recuperar la posición "+respuesta.length, Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
-
-
+	
     public class BuscarTask extends AsyncTask<Void, Void, Void>{
 
         @Override
@@ -191,17 +155,7 @@ public class MainActivity extends Activity
 
   public class EnvioRecepcionSms extends AsyncTask<Void, Void, Void>{
 
-        @Override
-        protected void onPreExecute(){
-            Toast.makeText(getApplicationContext(), " Enviando petición ...", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            // TODO Auto-generated method stub
-            try {
-
-                final Thread t = new Thread(){
+        final Thread tProgress = new Thread(){
                     @Override
                     public void run() {
                         try {
@@ -224,10 +178,17 @@ public class MainActivity extends Activity
                     }
                 };
 
-                t.start();
-                Thread.sleep(20000);
-                //Thread.sleep(3000);
-                //smsRespuesta = cgeneral.readSMS();
+        @Override
+        protected void onPreExecute(){
+            //Iniciando
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // TODO Auto-generated method stub
+            try {
+                tProgress.start();
+                Thread.sleep(20000); // 20 Segundos
             } catch (Exception e) {
                 Toast.makeText(getApplicationContext(), "Error al leer Mensaje", Toast.LENGTH_SHORT).show();
             }
@@ -237,6 +198,7 @@ public class MainActivity extends Activity
 
         @Override
         protected void onPostExecute(Void aVoid){
+            
             smsRespuesta = cgeneral.readSMS();
             String[] respuesta = smsRespuesta.split("\\|");
             if(respuesta.length == 3){
@@ -244,6 +206,7 @@ public class MainActivity extends Activity
                 latitud = respuesta[1];
                 longitud = respuesta[2];
                 if(numeroEnvio.trim().equalsIgnoreCase(numeroRetorno.trim())) {
+                    
                     Uri location = Uri.parse("geo:<"+latitud+">, <"+longitud+">?q=<"+latitud + ">, <" + longitud +">(Posición)");
                     Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
 
@@ -259,27 +222,12 @@ public class MainActivity extends Activity
                   Toast.makeText(getApplicationContext(), "La respuesta no es del cliente solicitado", Toast.LENGTH_SHORT).show();
             }
             else
-              Toast.makeText(getApplicationContext(), "Se excedio el tiempo de respuesta no se puedo recuperar la posición ", Toast.LENGTH_SHORT).show();
+              Toast.makeText(getApplicationContext(), ":( Se excedio el tiempo de respuesta no se puedo recuperar la posición ", Toast.LENGTH_SHORT).show();
 
         }
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.item) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    
+    private void finish(View v){
+        finish();
     }
 }
