7f5810ebd34f2defe7e5a71ee5675cb83546b2ae
cnvill <cn_vill@hotmail.com>
2015-03-03 21:05:58 +0000

    Add Option Config

--- NotifyServer/gen/com/cn/notifyserver/R.java
+++ NotifyServer/gen/com/cn/notifyserver/R.java
@@ -24,8 +24,9 @@ public final class R {
         public static final int txtNumeroContacto=0x7f070005;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int newcontact=0x7f030001;
+        public static final int configmain=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int newcontact=0x7f030002;
     }
     public static final class menu {
         public static final int menu=0x7f060000;
@@ -33,6 +34,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int title_configmain=0x7f040003;
         public static final int title_newcontact=0x7f040002;
     }
     public static final class style {
---
+++ NotifyServer/src/com/cn/notifyserver/ConfigMain.java
@@ -0,0 +1,48 @@
+package com.cn.notifyserver;
+
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.widget.AdapterView.OnItemClickListener;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.database.Cursor;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.AsyncTask;
+import com.cn.notifyserver.BD.DataBaseManager;
+import com.cn.notifyserver.Class.GeneralCn;
+import java.util.List;
+public class ConfigMain extends Activity
+{
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configmain);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.item) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
